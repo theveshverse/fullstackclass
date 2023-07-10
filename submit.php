134da2649c46b0c46e6efca3a73ae6828ac7595f
theveshverse <104341626+theveshverse@users.noreply.github.com>
2023-07-10 11:44:01 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Thank You</title>
+  <link rel="stylesheet" type="text/css" href="survey.css">
+</head>
+<body>
+  <main>
+    <h1>Thank You!</h1>
+    <p>Thank you for taking the time to complete the survey. Your responses have been recorded.</p>
+  </main>
+</body>
+</html>
